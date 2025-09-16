69bc364929dd1758e3c6f6df0b3b44e290f5f0f1
kirubhanandhan <viranandhu@gmail.com>
2025-09-17 00:35:41 +0530

    pwa added

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,20 @@ const eventRoutes = require('./event-routes/event-routes');
 const cors = require('cors');
 
 const app = express();
+
+const path = require("path");
+
 const port = process.env.PORT || 1997;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../dist/frontend/browser")));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/frontend/browser/index.html'));
+});
+
 
 app.use(cors()); 
 
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,10 +6,14 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),
-  provideHttpClient(),importProvidersFrom(MatPaginatorModule)
+  provideHttpClient(),importProvidersFrom(MatPaginatorModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
